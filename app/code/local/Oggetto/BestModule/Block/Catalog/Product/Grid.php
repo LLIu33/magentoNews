dce3211c14f04cb5f434111fb9179ae766dc9fcd
Artem G <tuocs@yandex.ru>
2017-12-15 14:03:46 +0300

    Rework adding is_best attribute to product list
    [re #53125]

---
+++ app/code/local/Oggetto/BestModule/Block/Catalog/Product/Grid.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Oggetto Web extension for Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * the Oggetto BestModule module to newer versions in the future.
+ * If you wish to customize the Oggetto BestModule module for your needs
+ * please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Oggetto
+ * @package    Oggetto_BestModule
+ * @copyright  Copyright (C) 2017 Oggetto Web ltd (http://oggettoweb.com/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Oggetto BestModule
+ *
+ * @category   Oggetto
+ * @package    Oggetto_BestModule
+ * @subpackage Block
+ * @author     Artem Grechko <agrechko@oggettoweb.com>
+ */
+class Oggetto_BestModule_Block_Catalog_Product_Grid
+    extends Mage_Adminhtml_Block_Catalog_Product_Grid
+{
+    /**
+     * Prepare columns
+     *
+     * @return Mage_Adminhtml_Block_Catalog_Product_Grid
+     */
+    protected function _prepareColumns()
+    {
+        $values = Mage::getModel('Oggetto_BestModule_Model_Attribute_Source_Option')->toArray();
+        $this->addColumnAfter(
+            'is_best',
+            [
+                'header' => Mage::helper('bestmodule')->__('Is Best'),
+                'index'  => 'is_best',
+                'type'   => 'options',
+                'options' => $values,
+            ],
+            'entity_id'
+        );
+        return parent::_prepareColumns();
+    }
+
+    /**
+     * Modifying setter collection
+     *
+     * @param Varien_Data_Collection $collection
+     */
+    public function setCollection($collection)
+    {
+        $collection->addAttributeToSelect('is_best');
+        parent::setCollection($collection);
+    }
+}
--- app/code/local/Oggetto/BestModule/Model/Observer.php
+++ app/code/local/Oggetto/BestModule/Model/Observer.php
@@ -63,58 +63,7 @@ class Oggetto_BestModule_Model_Observer extends Varien_Event_Observer
             }
         }
     }
-
-    /**
-     * Add is_best column to product grid
-     *
-     * @param Varien_Event_Observer $observer
-     * @event core_block_abstract_to_html_before
-     * @return void
-     */
-    public function beforeBlockToHtml(Varien_Event_Observer $observer)
-    {
-        $grid = $observer->getBlock();
-
-        $values = Mage::getModel('Oggetto_BestModule_Model_Attribute_Source_Option')->toArray();
-
-        if ($grid instanceof Mage_Adminhtml_Block_Catalog_Product_Grid) {
-            /* @var Mage_Adminhtml_Block_Catalog_Product_Grid $grid */
-            $grid->addColumnAfter(
-                'is_best',
-                [
-                    'header' => Mage::helper('bestmodule')->__('Is Best'),
-                    'index'  => 'is_best',
-                    'type'   => 'options',
-                    'options' => $values,
-                ],
-                'entity_id'
-            );
-        }
-    }
-
-    /**
-     * Add is_best data to product collection
-     *
-     * @param Varien_Event_Observer $observer
-     * @event eav_collection_abstract_load_before
-     * @return void
-     */
-    public function beforeCollectionLoad(Varien_Event_Observer $observer)
-    {
-        $collection = $observer->getCollection();
-        if (!isset($collection)) {
-            return;
-        }
-
-        /**
-         * Mage_Customer_Model_Resource_Customer_Collection
-         */
-        if ($collection instanceof Mage_Catalog_Model_Resource_Product_Collection) {
-            /* @var $collection Mage_Catalog_Model_Resource_Product_Collection */
-            $collection->addAttributeToSelect('is_best');
-        }
-    }
-
+    
     /**
      * Set is_best for quoteItem
      *
